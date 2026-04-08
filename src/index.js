a2f9ea1182c822069372b33ad9561ecd7b70a565
noireko <agustinfernandezrolon@gmail.com>
2026-04-07 23:54:33 -0300

    Added event listener for function import

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const btnMenu = document.getElementById("btn-menu");
 const btnHome = document.getElementById("btn-home");
+const btnContacts = document.getElementById("btn-contactos");
+
 
 import "./styles.css";
 import firstLoad from "./firstload.js";
 import cargarMenu from "./menu.js";
 import cargarHome from "./cargarHome.js";
+import cargarContact from "./cargarContacts.js";
 
 firstLoad();
 btnMenu.addEventListener("click", cargarMenu)
 btnHome.addEventListener("click", cargarHome)
+btnContacts.addEventListener("click", cargarContact)
