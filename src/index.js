d5b7d6f181309a380c2c81f27d5bfc67086e3b2a
noireko <agustinfernandezrolon@gmail.com>
2026-04-07 23:26:34 -0300

    added cargarHome and cargarMenu eventlisteners and imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+const btnMenu = document.getElementById("btn-menu");
+const btnHome = document.getElementById("btn-home");
+
 import "./styles.css";
 import firstLoad from "./firstload.js";
+import cargarMenu from "./menu.js";
+import cargarHome from "./cargarHome.js";
 
 firstLoad();
+btnMenu.addEventListener("click", cargarMenu)
+btnHome.addEventListener("click", cargarHome)
